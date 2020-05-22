bc4256c1b52dbf8b94ba2efbb8766b4a70c79752
David Aronchick <aronchick@gmail.com>
2020-05-22 02:08:12 +0000

    package step built and passing unit tests

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM mlspec/mlspeclib-action-docker@sha256:c13eac93072c5d7f5582bd89bef72d0ef9518a3f263489514e146bca3a9e612d
+
+RUN apt-get -y update && apt-get -y install python3-all python3-pip
+
+COPY requirements.txt /requirements.txt
+
+ARG FIRSTCACHEBUST=1
+
+RUN python3 -m pip install -U pip
+RUN python3 -m pip install --no-cache-dir -r /requirements.txt
+
+COPY .parameters/schemas /src/parameters
+COPY integration/.parameters/schemas /src/parameters/test_schemas
+COPY step_execution.py /src
+COPY integration/container_debugging.sh /src
+
+ENTRYPOINT ["./entrypoint.sh"]
---
+++ step_execution.py
@@ -0,0 +1,67 @@
+import logging
+from mlspeclib import MLObject
+from pathlib import Path
+import yaml as YAML
+
+# Making this a class in case we want sub functions.
+class StepExecution:
+    input_params = {}  # noqa
+    execution_params = {}  # noqa
+    ml_object = MLObject()  # noqa
+    logger = None  # noqa
+
+    def __init__(self, input_params, execution_params):
+        self.input_params = input_params
+        self.execution_params = execution_params
+        self.logger = logging.getLogger()
+
+        # Execute all work in here.
+
+        # Output input params & execution params
+        if self.input_params is not None:
+            self.logger.debug(f"Input params: {self.input_params}")
+
+        if self.execution_params is not None:
+            self.logger.debug(f"Execution params: {self.execution_params}")
+
+    def execute(self, result_object_schema_type, result_object_schema_version):
+        # Create Result object
+        results_object = MLObject()
+        results_object.set_type(
+            schema_type=result_object_schema_type,
+            schema_version=result_object_schema_version,
+        )
+
+        # Mocked up results
+        return_dict = YAML.safe_load(
+            """
+servable: True
+package_size: 1029310298
+tested_platforms: ['kubeflow', 'azureml', 'sagemaker']
+model_source:
+    servable_model:
+        data_store: 's3'
+        bucket: 'nlp-bucket'
+        path: 'a231653454ca8e07f42adc7941aeec6b'
+serving_container_image:
+    container_image_url: 'https://hub.docker.com/repository/docker/contoso/nlp-base-images'
+"""
+        )
+
+        results_object.servable = return_dict["servable"]
+        results_object.tested_platforms = return_dict["tested_platforms"]
+        results_object.package_size = return_dict["package_size"]
+        results_object.model_source.servable_model.data_store = return_dict[
+            "model_source"
+        ]["servable_model"]["data_store"]
+        results_object.model_source.servable_model.bucket = return_dict["model_source"][
+            "servable_model"
+        ]["bucket"]
+        results_object.model_source.servable_model.path = return_dict["model_source"][
+            "servable_model"
+        ]["path"]
+        results_object.serving_container_image.container_image_url = return_dict[
+            "serving_container_image"
+        ]["container_image_url"]
+
+        return results_object
